c4e215519370f8fff8ad6e6b653928938a18631f
AndreaUni <andrea.formichetti@student.univaq.it>
2020-07-22 11:46:12 +0200

    hhasdaoih

--- src/main/java/proxys/Immagine_Proxy.java
+++ src/main/java/proxys/Immagine_Proxy.java
@@ -25,7 +25,7 @@ public class Immagine_Proxy extends Immagine_Imp implements Data_ItemProxy{
     protected DataLayer dataLayer;
     protected boolean dirty;
         
-    public Immagine_Proxy(DataLayer d) {
+    public Immagine_Proxy(DataLayer d) {aggiunto su master
         super();
         
         this.dataLayer = d;
